68a632809cb063b5d0d161b77f9bce9a84649c52
Murty2503 <141997030+Murty2503@users.noreply.github.com>
2023-09-29 20:44:44 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,7 +20,7 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+#streamlit.text(fruityvice_response.json())
 
 # the below line is to flatten json and assign it to a pandas variable called fruityvice_normalized
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
