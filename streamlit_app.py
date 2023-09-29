bade4638e0df24ce385320cf230765b2aa9827c1
Murty2503 <141997030+Murty2503@users.noreply.github.com>
2023-09-29 23:28:40 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,9 @@
 import streamlit
 import pandas
+import snowflake.connector
+import requests
+from urllib.error import URLError
+
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
@@ -21,14 +25,13 @@ streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
 
-import requests
 #streamlit.text(fruityvice_response.json())
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 # the below line is to flatten json and assign it to a pandas variable called fruityvice_normalized
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # this below line is to convert the json data into tabular form
 streamlit.dataframe(fruityvice_normalized)
-
+streamlit.stop()
 import snowflake.connector
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
